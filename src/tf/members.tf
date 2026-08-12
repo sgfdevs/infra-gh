05cf69c7e6a850e5a15d7a25ea46d7ad2d845dcf
Levi Zitting <me@levizitting.com>
2026-08-12 01:40:16 -0500

    refactor: group organization membership roles

--- src/tf/members.tf
+++ src/tf/members.tf
@@ -1,14 +1,21 @@
+locals {
+  organization_admins = toset([
+    "glitchedmob",
+    "jenn8421",
+    "Motter360",
+    "Ryan-DL",
+  ])
+
+  organization_members = toset([
+    "fredlawl",
+    "jason-klein",
+    "sm0862512",
+  ])
+}
+
 resource "github_membership" "organization" {
-  for_each = {
-    fredlawl    = "member"
-    glitchedmob = "admin"
-    jason-klein = "member"
-    jenn8421    = "admin"
-    Motter360   = "admin"
-    Ryan-DL     = "admin"
-    sm0862512   = "member"
-  }
+  for_each = setunion(local.organization_admins, local.organization_members)
 
   username = each.key
-  role     = each.value
+  role     = contains(local.organization_admins, each.key) ? "admin" : "member"
 }
