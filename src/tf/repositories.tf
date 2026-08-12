afc6c9c6c8b829bb160992fd8a6a999b047606b3
Levi Zitting <me@levizitting.com>
2026-08-12 08:15:58 -0500

    feat: manage infrastructure repositories

--- src/tf/repositories.tf
+++ src/tf/repositories.tf
@@ -1,10 +1,38 @@
-module "infra_dns" {
+locals {
+  github_actions_integration_id = 15368
+
+  infrastructure_repositories = {
+    infra-app-config = {
+      required_check = "Terraform checks"
+    }
+    infra-aws-core = {
+      required_check = "Terraform checks"
+    }
+    infra-dns = {
+      required_check = "Terraform checks"
+    }
+    infra-gh = {
+      required_check = "Terraform checks"
+    }
+    infra-k8s-apps = {
+      required_check = "checks / Kubernetes checks"
+    }
+    infra-vm-workloads = {
+      required_check = "CI checks"
+    }
+  }
+}
+
+module "infrastructure_repository" {
+  for_each = local.infrastructure_repositories
+
   source = "./modules/github-repository"
 
-  name = "infra-dns"
+  name   = each.key
+  topics = ["infra"]
   required_checks = [{
-    context        = "Terraform checks"
-    integration_id = 15368
+    context        = each.value.required_check
+    integration_id = local.github_actions_integration_id
   }]
   bypass_actors = [{
     actor_id   = github_team.platform_admins.id
--- src/tf/repository-access.tf
+++ src/tf/repository-access.tf
@@ -1,27 +1,16 @@
-locals {
-  infrastructure_repositories = toset([
-    "infra-app-config",
-    "infra-aws-core",
-    "infra-dns",
-    "infra-gh",
-    "infra-k8s-apps",
-    "infra-vm-workloads",
-  ])
-}
-
 resource "github_team_repository" "infra_platform_admins" {
-  for_each = local.infrastructure_repositories
+  for_each = module.infrastructure_repository
 
   team_id    = github_team.platform_admins.id
-  repository = each.value
+  repository = each.value.name
   permission = "admin"
 }
 
 resource "github_team_repository" "infra_maintainers" {
-  for_each = local.infrastructure_repositories
+  for_each = module.infrastructure_repository
 
   team_id    = github_team.infra_maintainers.id
-  repository = each.value
+  repository = each.value.name
   permission = "maintain"
 }
 
